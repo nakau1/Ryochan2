8ed218455a7a481754b8b5fe55e454e8851f65db
中安佑一 <yuichi.nakayasu@gmail.com>
2018-05-16 00:36:17 +0900

    ブートストラップの定義

--- Ryochan2/Sources/Screen/BootstrapViewController.swift
+++ Ryochan2/Sources/Screen/BootstrapViewController.swift
@@ -6,4 +6,53 @@ import UIKit
 
 class BootstrapViewController: UITableViewController {
     
+    // ここで動作を定義します
+    // =========================================================================
+    private let items: [(section: String, rows: [(title: String, handler: (UIViewController)->Void)])] = [
+        (section: "App",
+         rows: [
+            (title: "アプリ起動", handler: { vc in
+                vc.present(LaunchViewController.create(), animated: true) {}
+            }),
+            ]),
+        (section: "各画面テスト",
+         rows: [
+            (title: "トップ画面", handler: { vc in
+                
+            }),
+            (title: "リスト画面", handler: { vc in
+                
+            }),
+            ]),
+        (section: "簡易テスト",
+         rows: [
+            (title: "機能テスト", handler: { vc in
+                
+            }),
+            ]),
+        ]
+    // =========================================================================
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items[section].rows.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.section].rows[indexPath.row].title
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        items[indexPath.section].rows[indexPath.row].handler(self)
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return items[section].section
+    }
 }
