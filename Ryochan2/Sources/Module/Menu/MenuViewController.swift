0d961f10eff396bfdd7bc6bc4db58858d8b0c694
中安佑一 <yuichi.nakayasu@gmail.com>
2018-05-16 00:56:24 +0900

    モジュール系コントローラの追加

---
+++ Ryochan2/Sources/Module/Menu/MenuViewController.swift
@@ -0,0 +1,17 @@
+// =============================================================================
+//  Ryochan
+//  Copyright Yuichi Nakayasu. All rights reserved.
+// =============================================================================
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    class func create() -> UIViewController {
+        let vc = instantiate(self)
+        return vc
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Ryochan2/Sources/Module/TextInput/TextInputViewController.swift
@@ -0,0 +1,17 @@
+// =============================================================================
+//  Ryochan
+//  Copyright Yuichi Nakayasu. All rights reserved.
+// =============================================================================
+import UIKit
+
+class TextInputViewController: UIViewController {
+    
+    class func create() -> UIViewController {
+        let vc = instantiate(self)
+        return vc
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
