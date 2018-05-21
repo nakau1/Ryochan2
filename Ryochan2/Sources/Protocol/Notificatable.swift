bd3366f1bba78e284c1ba64173929ee16ba99699
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-05-21 11:51:59 +0900

    Notificatableの実装

--- Ryochan2/Sources/Protocol/Notificatable.swift
+++ Ryochan2/Sources/Protocol/Notificatable.swift
@@ -4,6 +4,26 @@
 // =============================================================================
 import UIKit
 
-protocol Notificatable {
+protocol Notificatable {}
     
+extension Notificatable where Self: NSObject {
+    
+    /// 通知の監視を開始する
+    ///
+    /// - Parameters:
+    ///   - name: 監視する通知名
+    ///   - selector: 通知された時に呼ばれるセレクタ
+    func observeNotification(_ name: Notification.Name, when selector: Selector) {
+        NotificationCenter.default.addObserver(self, selector: selector, name: name, object: nil)
+    }
+}
+
+extension Notificatable {
+    
+    /// 通知を送信する
+    ///
+    /// - Parameter name: 送信する通知名
+    func postNotification(_ name: Notification.Name, userInfo: [AnyHashable : Any]? = nil) {
+        NotificationCenter.default.post(name: name, object: nil, userInfo: userInfo)
+    }
 }
