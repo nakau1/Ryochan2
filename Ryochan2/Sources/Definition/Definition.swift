79117aa0115fc4a3d08ac36def1130436f08ce80
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-05-23 14:26:09 +0900

    ドキュメントディレクトリ取得用プロパティ追加

--- Ryochan2/Sources/Definition/Definition.swift
+++ Ryochan2/Sources/Definition/Definition.swift
@@ -12,4 +12,8 @@ struct Const {
 /// ファイルパス
 struct Path {
     
+    /// ドキュメントディレクトリ
+    static var documentDirectory: String {
+        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+    }
 }
