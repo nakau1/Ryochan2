8d0ac1d52efe0017fcf1e346a2a6e1cf06a35448
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-05-25 12:16:56 +0900

    フォーメーションマネージャの実装

--- Ryochan2/Sources/Model/Manager/FormationManager.swift
+++ Ryochan2/Sources/Model/Manager/FormationManager.swift
@@ -6,4 +6,40 @@ import UIKit
 
 class FormationManager {
     
+    private struct JsonCoder: JsonEncodable, JsonDecodable {}
+    
+    private var stored: Formation!
+    private var jsonCoder = JsonCoder()
+    
+    // MARK: - list -
+    
+    /// ファイルに保存する
+    ///
+    /// - Parameter formation: フォーメーションオブジェクト
+    func save(_ formation: Formation) {
+        jsonCoder.saveJson(formation, to: Path.Formation.json)
+    }
+    
+    /// ファイルに保存された一覧を読み込む
+    ///
+    /// - Returns: 似顔絵オブジェクトの配列
+    func load() -> Formation {
+        if let formation = stored { return formation }
+        
+        if File.makeTextFileIfNeeded(to: Path.Formation.json) {
+            makeDefaultFormation()
+        }
+        stored = jsonCoder.loadJson(path: Path.Formation.json, to: Formation.self, default: instantiateDefaultFormation())
+        return stored
+    }
+    
+    private func makeDefaultFormation() {
+        save(instantiateDefaultFormation())
+    }
+    
+    private func instantiateDefaultFormation() -> Formation {
+        let formation = jsonCoder.instantiate(decodableType: Formation.self)
+        formation.startings = PositionTemplate.pos442a.positions
+        return formation
+    }
 }
