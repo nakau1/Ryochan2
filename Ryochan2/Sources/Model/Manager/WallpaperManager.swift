5e42e3a52871abcaad738f9eea62ed813d240caa
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-05-25 17:33:39 +0900

    壁髪マネージャの実装

--- Ryochan2/Sources/Model/Manager/WallpaperManager.swift
+++ Ryochan2/Sources/Model/Manager/WallpaperManager.swift
@@ -6,15 +6,47 @@ import UIKit
 
 class WallpaperManager: JsonDecodable {
     
-    func list() -> [Wallpaper] {
-        return loadJson(path: "", to: [Wallpaper].self, default: [])
+    private struct JsonCoder: JsonEncodable, JsonDecodable {}
+    
+    private var storedList: [Wallpaper]!
+    private var jsonCoder = JsonCoder()
+    
+    /// 一覧をファイルに保存する
+    ///
+    /// - Parameter list: 似顔絵オブジェクトの配列
+    private func saveList(_ list: [Wallpaper]) {
+        jsonCoder.saveJson(list, to: Path.Wallpaper.json)
     }
     
-    func thumbImages() -> [UIImage?] {
-        return []
+    /// ファイルに保存された一覧を読み込む
+    ///
+    /// - Returns: 似顔絵オブジェクトの配列
+    func loadList() -> [Wallpaper] {
+        if let list = storedList { return list }
+        
+        _ = File.makeTextFileIfNeeded(to: Path.Wallpaper.json)
+        storedList = jsonCoder.loadJson(path: Path.Wallpaper.json, to: [Wallpaper].self, default: [])
+        return storedList
     }
+    
+    /// サムネイル画像の一覧を取得する
+    ///
+    /// - Returns: サムネイル画像の一覧
+    func loadThumbList() -> [UIImage?] {
+        return loadList().map { wallpaper -> UIImage? in
+            return UIImage(path: Path.Wallpaper.thumb(of: wallpaper))
+        }
+    }
+    
+    // MARK: - write image -
     
     func writeImages(of wallpaper: Wallpaper) {
-        
+        writeThumbImage(of: wallpaper)
+    }
+    
+    private func writeThumbImage(of wallpaper: Wallpaper) {
+        _ = File.makeDirectoryIfNeeded(to: Path.Wallpaper.directory)
+        // TODO: 未実装
+//        generateImage().write(to: Path.portraitImage(self))
     }
 }
