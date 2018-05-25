dfd9555ed7685cae8ab550f3c983017e185d3d64
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-05-25 17:57:09 +0900

    SNS共有クラスの実装

--- Ryochan2/Sources/Model/Sns.swift
+++ Ryochan2/Sources/Model/Sns.swift
@@ -4,6 +4,25 @@
 // =============================================================================
 import UIKit
 
+/// SNS共有用クラス
 class Sns {
     
+    /// SNSに共有する画面を表示する
+    ///
+    /// - Parameters:
+    ///   - viewController: 表示元の画面
+    ///   - items: 共有に含めるアイテムの配列
+    class func show(from viewController: UIViewController, items: [Any]) {
+        let vc = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        viewController.present(vc, animated: true, completion: nil)
+    }
+    
+    /// 画像をSNSに共有する画面を表示する
+    ///
+    /// - Parameters:
+    ///   - viewController: 表示元の画面
+    ///   - image: 画像
+    class func show(from viewController: UIViewController, image: UIImage) {
+        show(from: viewController, items: [" #似顔絵アプリ", image])
+    }
 }
