a188fb8dc4a7f0a5d1155c4821040b5424c953be
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-05-29 19:48:27 +0900

    パーツの種別を追加

---
+++ Ryochan2/Sources/Model/Enum/PartsKind.swift
@@ -0,0 +1,15 @@
+// =============================================================================
+//  Ryochan
+//  Copyright Yuichi Nakayasu. All rights reserved.
+// =============================================================================
+import UIKit
+
+/// パーツの種別
+enum PartsKind {
+    
+    /// 似顔絵
+    case portrait
+    
+    /// ユニフォーム
+    case uniform
+}
